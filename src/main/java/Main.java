cf111b70d6ce9738fb363d023b14c43f79119d04
Dragos Mihalcea <dragos.mihalcea@luxon-tech.com>
2026-01-29 14:58:41 +0200

    Added the tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
+  public static void main(String[] args) {
+
+    LengthEncoder le1 = new LengthEncoder("wwwwaaadexxxxxx");
+    LengthEncoder le2 = new LengthEncoder("aaaabbbccc");
+    LengthEncoder le3 = new LengthEncoder("abbbcdddd");
+    LengthEncoder le4 = new LengthEncoder("aabcccaazzzz");
+
+    System.out.println("Pre-Tests:");
+    System.out.println(le1.encode());
+    System.out.println(le2.encode());
+    System.out.println(le3.encode());
+    System.out.println(le4.encode());
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your test:");
+    String keyboardInput = sc.nextLine();
+
+    LengthEncoder le5 = new LengthEncoder(keyboardInput);
+    System.out.println(le5.encode());
+
+  }
 }

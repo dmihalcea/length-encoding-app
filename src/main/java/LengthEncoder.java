dbd053068d0482734fb9a5fd4dc69716e4452285
Dragos Mihalcea <dragos.mihalcea@luxon-tech.com>
2026-01-29 14:43:17 +0200

    Added the functionality for LengthEncoder

--- src/main/java/LengthEncoder.java
+++ src/main/java/LengthEncoder.java
@@ -1,3 +1,34 @@
+/**
+ * Responsible for encoding the length of a String
+ */
 public class LengthEncoder {
 
+  public final String input;
+
+  public LengthEncoder(String input) {
+    this.input = input;
+  }
+
+  public String encode() {
+    String str = this.input;
+
+    if (str == null || str.isEmpty()) {
+      return "";
+    }
+
+    StringBuilder result = new StringBuilder();
+    int cnt = 1;
+
+    for (int i = 1; i <= str.length(); i++) {
+      if (i < str.length() && str.charAt(i) == str.charAt(i - 1)) {
+        cnt++;
+      } else {
+        result.append(str.charAt(i - 1));
+        result.append(cnt);
+        cnt = 1;
+      }
+    }
+
+    return result.toString();
+  }
 }
